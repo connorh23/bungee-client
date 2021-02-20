48b7019a7a7b6305e3dcfd1fd33f081b6871f815
Connor Hood <connor.hood234@mail.com>
2021-02-20 15:26:36 -0800

    Use bungee-lib environment & http module structure

--- rest.js
+++ rest.js
@@ -1,27 +1,34 @@
-const { requests } = require('bungee-lib/http');
-const { env_vars } = require('./environment');
+
+const {
+    environment
+} = require('bungee-lib');
+
+const { 
+    requests,
+    responses
+ } = require('bungee-lib/http');
 
 const create = async ({ model, item }) => {
-    const url = `${env_vars.API_DOMAIN}/rest/${model}/create`;
+    const url = `${environment.vars.API_DOMAIN}/rest/${model}/create`;
     const response =  await requests.post({ url, request_body: item });
     return __parse_response(response);
 };
 
 const retrieve = async ({ model, id }) => {
-    const url = `${env_vars.API_DOMAIN}/rest/${model}/retrieve/${id}`;
+    const url = `${environment.vars.API_DOMAIN}/rest/${model}/retrieve/${id}`;
     const response =  await requests.get({ url });
     return __parse_response(response);
 };
 
 const update = async ({ model, item }) => {
-    const url = `${env_vars.API_DOMAIN}/rest/${model}/update`;
+    const url = `${environment.vars.API_DOMAIN}/rest/${model}/update`;
     const response =  await requests.put({ url, request_body: item });
     const raw_update_response = __parse_response(response);
     return { ... raw_update_response, data: raw_update_response.data[1] }
 };
 
 const destroy = async ({ model, id }) => {
-    const url = `${env_vars.API_DOMAIN}/rest/${model}/destroy/${id}`;
+    const url = `${environment.vars.API_DOMAIN}/rest/${model}/destroy/${id}`;
     const response = await requests.destroy({ url });
     return __parse_response(response);
 };
