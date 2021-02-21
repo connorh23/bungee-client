3b6143a3758f159e5ad4671d3152d99cab699cd6
Connor Hood <connor.hood234@mail.com>
2021-02-20 18:08:56 -0800

    Create ModelClient class

---
+++ model.js
@@ -0,0 +1,34 @@
+const { rest } = require('./rest');
+
+class ModelClient {
+
+   MODEL_NAME;
+
+   constructor({ model_name }) {
+      this.MODEL_NAME = model_name;
+   }
+
+   create = async item => {
+      let response = await rest.create({ model: this.MODEL_NAME, item });
+      return response.data;
+   };
+
+   retrieve = async id => {
+      let response = await rest.retrieve({ model: this.MODEL_NAME, id });
+      return response.data;
+   };
+
+   update = async item => {
+      let response = await rest.update({ model: this.MODEL_NAME, item });
+      return response.data;
+   };
+
+   destroy = async id => {
+      let response = await rest.destroy({ model: this.MODEL_NAME, id });
+      return response.data === 1;
+   };
+}
+
+module.exports = {
+   ModelClient
+};
