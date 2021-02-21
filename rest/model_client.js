89de806992650c50444ca77a6eb28c52759fe415
Connor Hood <connor.hood234@mail.com>
2021-02-20 19:22:28 -0800

    Restore full class-based client

--- rest/model_client.js
+++ rest/model_client.js
@@ -1,30 +1,30 @@
 const rest = require('./rest');
 
-let MODEL_NAME;
-
 class ModelClient {
 
+   MODEL_NAME;
+
    constructor({ model_name }) {
-      MODEL_NAME = model_name;
+      this.MODEL_NAME = model_name;
    }
 
    create = async item => {
-      let response = await rest.create({ model: MODEL_NAME, item });
+      let response = await rest.create({ model: this.MODEL_NAME, item });
       return response.data;
    };
 
    retrieve = async id => {
-      let response = await rest.retrieve({ model: MODEL_NAME, id });
+      let response = await rest.retrieve({ model: this.MODEL_NAME, id });
       return response.data;
    };
 
    update = async item => {
-      let response = await rest.update({ model: MODEL_NAME, item });
+      let response = await rest.update({ model: this.MODEL_NAME, item });
       return response.data;
    };
 
    destroy = async id => {
-      let response = await rest.destroy({ model: MODEL_NAME, id });
+      let response = await rest.destroy({ model: this.MODEL_NAME, id });
       return response.data === 1;
    };
 }
